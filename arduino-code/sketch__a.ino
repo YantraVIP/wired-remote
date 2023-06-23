cd1879b887fd5c60edea10be12a4ccd07e880165
YantraVIP <137476180+YantraVIP@users.noreply.github.com>
2023-06-23 10:51:45 +0530

    Update sketch__a.ino

--- arduino-code/sketch__a.ino
+++ arduino-code/sketch__a.ino
@@ -1,1 +1,36 @@
+int enableAPin = 3;
+int in1Pin = 4;
+int in2Pin = 5;
 
+void setup()
+{
+  pinMode(enableAPin, OUTPUT);
+  pinMode(in1Pin, OUTPUT);
+  pinMode(in2Pin, OUTPUT);
+}
+
+void loop()
+{
+  int motorPWMSpeed = 0;
+  int joystickValue = analogRead(A0);
+
+  if (joystickValue >= 530)  
+  {
+    motorPWMSpeed = map(joystickValue, 530, 1023, 0, 255);
+    digitalWrite(in1Pin, HIGH);
+    digitalWrite(in2Pin, LOW);    
+    analogWrite(enableAPin, motorPWMSpeed);
+  }
+  else if (joystickValue <= 490)    
+  {
+    motorPWMSpeed = map(joystickValue, 490, 0, 0, 255);
+    digitalWrite(in1Pin, LOW);
+    digitalWrite(in2Pin, HIGH);    
+    analogWrite(enableAPin, motorPWMSpeed);
+  }  
+  else
+  {
+    digitalWrite(in1Pin, LOW);
+    digitalWrite(in2Pin, LOW);    
+  }
+}
